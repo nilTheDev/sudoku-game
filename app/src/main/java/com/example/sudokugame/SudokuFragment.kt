946dc8cd117e282ad28596c2ed88a68cfeff5d63
nilthedev <nilthedev@gmail.com>
2021-06-20 11:44:56 +0530

    partially applied getRow function in the SudokuFragment.kt

--- app/src/main/java/com/example/sudokugame/SudokuFragment.kt
+++ app/src/main/java/com/example/sudokugame/SudokuFragment.kt
@@ -5,6 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.annotation.DimenRes
+import androidx.core.view.marginStart
+import androidx.core.view.marginTop
+import android.view.ViewGroup.LayoutParams as GroupLayoutParams
+import java.util.*
 
 
 class SudokuFragment : Fragment() {
@@ -17,5 +24,12 @@ class SudokuFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sudoku, container, false)
     }
 
+    private fun getRow(): LinearLayout {
+        val layout = LinearLayout(context)
+        layout.layoutParams = GroupLayoutParams(GroupLayoutParams.WRAP_CONTENT, GroupLayoutParams.WRAP_CONTENT)
+        layout.orientation = LinearLayout.HORIZONTAL
+
+
+    }
 
 }
