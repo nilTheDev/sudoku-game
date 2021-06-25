ca7bd0d13177095ed969f4ffa14db05c2b4c0a76
nilthedev <nilthedev@gmail.com>
2021-06-25 11:54:18 +0530

    refactored the checkNumber() function

--- app/src/main/java/com/example/sudokugame/Sudoku.kt
+++ app/src/main/java/com/example/sudokugame/Sudoku.kt
@@ -16,38 +16,56 @@ class Sudoku {
             setOf(1057, 1058, 1059, 1066, 1067, 1068, 1075, 1076, 1077),
             setOf(1060, 1061, 1062, 1069, 1070, 1071, 1078, 1079, 1080)
         )
-        fun checkNumber(sudokuParent: LinearLayout, cellId: Int, number: Int): Boolean{
-            // find the row and check it
+        fun isValidInput(sudokuParent: LinearLayout, cellId: Int, input: Int): Boolean{
+            // iterate the rows
             for(i in 1000..1080 step 9){
-                // find out the correct row
+                // whether the cellId in the current row
                 if(cellId in i..(i + 8)){
                     // as the row is found
-                    // check the current row
+                    // inspect the cells of the row
                     for(j in i..(i + 8)){
-                        if(sudokuParent.findViewById<TextView>(j).text == number.toString()) return false
+                        // skip the proposed cellId
+                        if (j == cellId) continue
+                        if(sudokuParent.findViewById<TextView>(j).text == input.toString()) return false
                     }
-                    // the row does not already have the number inputted into it
-                    // so break out of the loop to continue the inspection
+                    // the proposed input does not have any row-wise repetition
+                    // so break out from the loop to continue the inspection
                     // with the columns and squares
                     break
                 }
             }
 
-            // check the column
+            // iterate the columns
             for(i in 1000..1008){
+                // whether the cellId is in the current column
                 if(cellId in i..(i + 72) step 9){
+                    // as the column is found
+                    // inspect the cells of the entire column
                     for(j in i..(i + 72) step 9){
-                        if(sudokuParent.findViewById<TextView>(j).text == number.toString()) return false
+                        // skip the proposed cellId
+                        if(j == cellId) continue
+                        if(sudokuParent.findViewById<TextView>(j).text == input.toString()) return false
                     }
+                    // the proposed input does not have any column-wise repetition
+                    // so break out from the loop to continue the inspection
+                    // with the squares
                     break
                 }
             }
 
+            // iterate the squares
             for(square in squares){
+                // whether the cellId is in the current square
                 if(cellId in square){
+                    // inspect all the cells of the square
                     for(cell in square){
-                        if(sudokuParent.findViewById<TextView>(cell).text == number.toString()) return false
+                        // skip the proposed cellId
+                        if(cell == cellId) continue
+                        if(sudokuParent.findViewById<TextView>(cell).text == input.toString()) return false
                     }
+                    // as there is no repetition of the proposed input
+                    // in the square, break out from the loop
+                    // the input number is perfectly valid
                     break
                 }
             }
